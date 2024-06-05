a935e41e7352bbc814b9776a34282fe14383aa73
daihoc24 <hoangquy8259747@gmail.com>
2024-06-05 12:34:52 +0700

    80% ProductDetail

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'color1': '#87c84a',
+        'color2': 'rgb(113 113 113)',
+      },
+    },
   },
   plugins: [],
 }
