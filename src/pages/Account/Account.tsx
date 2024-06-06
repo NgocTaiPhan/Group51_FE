79763b85accb7600e1e9b6672e2a1c195b054211
Phan Ngọc Tài <ngoctaiphan.cv@gmail.com>
2024-06-07 02:35:28 +0700

    Hoàn thành trang Account với boostrap, antdesign

---
+++ src/pages/Account/Account.tsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import './Account.scss';
+import {UserInfor} from '../../components/InforUserForm/InforUser'
+
+
+export default function Account() {
+
+    return (
+        <UserInfor/>
+    );
+};
+
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,22 +1,36 @@
-import React, { lazy } from "react";
-import { useRoutes } from "react-router-dom";
+import React, {lazy} from "react";
+import {useRoutes} from "react-router-dom";
 import HomeLayout from "../layout/HomeLayout/HomeLayout";
 import ProductDetai from "../pages/ProductDetail/ProductDetail";
+import Account from "../pages/Account/Account";
 import Login from "../components/login/Login";
+
 export default function Router() {
-  const routing = useRoutes([
-    {
-      path: "/",
-      element: <HomeLayout />,
-      children: [
+    const routing = useRoutes([
         {
-          path: "/product-detail/:productId",
-          element: (
-              <ProductDetai />
-          ),
+            path: "/",
+            element: <HomeLayout/>,
+            children: [
+                {
+                    path: "/product-detail/:productId",
+                    element: (
+                        <ProductDetai/>
+                    ),
+                },
+            ],
         },
-      ],
-    },
-  ]);
-  return routing;
+        {
+            path: "/",
+            element:  <HomeLayout/>,
+            children: [
+                {
+                    path: "/account",
+                    element: (
+                        <Account/>
+                    ),
+                },
+            ],
+        },
+    ]);
+    return routing;
 }
